210b3b2b9f44464e54a386205716e4636cb938eb
deflatedlatte <deflatedlatte@gmail.com>
2025-01-06 00:29:51 +0900

    Change some parts of render_page example

--- src/ophinode/__main__.py
+++ src/ophinode/__main__.py
@@ -66,7 +66,10 @@ class MainPage(HTML5Page):
         )
 
     def head(self):
-        return []
+        return [
+            Meta(charset="utf-8"),
+            Title("Main Page")
+        ]
 
 if __name__ == "__main__":
     print(render_page(MainPage()))
