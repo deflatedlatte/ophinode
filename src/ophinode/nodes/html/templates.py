6331450f1cfa354e757095465bf5bdbe91e2eee8
deflatedlatte <deflatedlatte@gmail.com>
2026-02-02 01:01:17 +0900

    Refactor HTML5Page and HTML5Layout to pass context to methods

--- src/ophinode/nodes/html/templates.py
+++ src/ophinode/nodes/html/templates.py
@@ -7,13 +7,13 @@ class HTML5Page(Page):
     def layout(self):
         return HTML5Layout()
 
-    def html_attributes(self) -> dict:
+    def html_attributes(self, context: "ophinode.site.BuildContext") -> dict:
         return {}
 
-    def head(self):
+    def head(self, context: "ophinode.site.BuildContext"):
         return []
 
-    def body(self):
+    def body(self, context: "ophinode.site.BuildContext"):
         return []
 
 class HTML5Layout(Layout):
@@ -22,12 +22,12 @@ class HTML5Layout(Layout):
             HTML5Doctype(),
             Html(
                 Head(
-                    page.head()
+                    page.head(context)
                 ),
                 Body(
-                    page.body()
+                    page.body(context)
                 ),
-                **page.html_attributes()
+                **page.html_attributes(context)
             )
         ]
 
